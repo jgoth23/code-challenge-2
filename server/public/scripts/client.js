b369351301f5774fee1a73fab467950e845cfa62
Johnny Goth <Jgoth23@gmail.com>
2021-02-09 22:25:35 -0600

    object not working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,44 @@
 console.log('client.js sourced');
 
-$( document ).ready( onReady );
+$(document).ready(onReady);
 
 function onReady() {
-    console.log('DOM ready');
-    $('#addJokeButton').on('click')
-
+  console.log('DOM ready');
+  $('#addJokeButton').on('click', addJoke);
+  getJokes();
 }
+function getJokes() {
+  $.ajax({
+    type: 'GET',
+    url: '/jokes',
+  }).then(function (response) {
+    console.log(response);
+    for (let joke of response) {
+      $('#outputDiv').append(
+        '<div><h4>' +
+          joke.jokeQuestion +
+          '</h4><p>' +
+          joke.punchLine +
+          '</p><p><i>' +
+          joke.whoseJoke +
+          '</i></p></div>'
+      );
+    }
+  });
+}
+
 function addJoke() {
-  console.log('adding joke');
-  let jokes = [];
+  let newJoke = {
+    jokeQuestion: $('#questionIn').val(),
+    PunchLine: $('#punchlineIn').val(),
+    whose: $('#whoseJokeIn'),
+  };
+}
+
+$.ajax({
+  type: 'POST',
+  url: '/addJokes',
+  data: newJoke
+}).then(function (response) {
+  console.log(response);
+});
--- server/server.js
+++ server/server.js
@@ -1,58 +1,55 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express();
-const bodyParser = require( 'body-parser' );
+const bodyParser = require('body-parser');
 const PORT = 5000;
-const jokes = require('./modules/jokes');
 
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/jokes', function(req, res) {
-  console.log('GET request for jokes');
-  res.send(jokes.addJoke());
-})
-
-function addJoke() {
-let index = 0;
-
+// app.get('/jokes', function (req, res) {
+//   console.log('GET request for jokes');
+//   res.send(jokes.addJoke());
+//});
 
 let jokes = [
   {
-    whoseJoke: "Danny",
-    jokeQuestion: "Why do scuba divers fall backwards out of boats?",
-    punchLine: "If they fell forwards they’d still be in the boat!"
+    whoseJoke: 'Danny',
+    jokeQuestion: 'Why do scuba divers fall backwards out of boats?',
+    punchLine: 'If they fell forwards they’d still be in the boat!',
   },
   {
-    whoseJoke: "Luke",
-    jokeQuestion: "Two fish are in a tank. What did one fish say to the other?",
-    punchLine: "Do you know how to drive this thing?"
+    whoseJoke: 'Luke',
+    jokeQuestion: 'Two fish are in a tank. What did one fish say to the other?',
+    punchLine: 'Do you know how to drive this thing?',
   },
   {
-    whoseJoke: "Millie",
-    jokeQuestion: "What do you call a pile of cats?",
-    punchLine: "A meowntain!"
+    whoseJoke: 'Millie',
+    jokeQuestion: 'What do you call a pile of cats?',
+    punchLine: 'A meowntain!',
   },
   {
-    whoseJoke: "dEv",
-    jokeQuestion: "Why should you not play cards in the forest?",
-    punchLine: "Too many Cheetahs!"
+    whoseJoke: 'dEv',
+    jokeQuestion: 'Why should you not play cards in the forest?',
+    punchLine: 'Too many Cheetahs!',
   },
   {
-    whoseJoke: "Scott",
-    jokeQuestion: "I went to the zoo the other day, it had one dog...",
-    punchLine: "It was a shih tzu."
-  }
+    whoseJoke: 'Scott',
+    jokeQuestion: 'I went to the zoo the other day, it had one dog...',
+    punchLine: 'It was a shih tzu.',
+  },
 ];
 
-const result = jokeData[index];
-index += 1;
-index = index % jokesData.length;
-return result;
-}
-
-module.exports = addJoke;
 // serve back static files
 app.use(express.static('server/public'));
+
+app.get('/jokes', function(req, res) {
+  res.send(jokes);
+});
+
+app.post('/addJokes', function(req, res) {
+  console.log(req.body);
+  res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
   console.log('server running on: ', PORT);
